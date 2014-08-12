70491c814410b73270472196cec0354b2b657e8f
Omari Norman <omari@smileystation.com>
2014-08-11 21:23:10 -0400

    WIP

--- library/sunlight-test.hs
+++ library/sunlight-test.hs
@@ -1,1 +1,15 @@
-../sunlight-test.hs
+module Main where
+
+import Test.Sunlight
+
+inputs = TestInputs
+  { tiDescription = Nothing
+  , tiCabal = "cabal"
+  , tiLowest = ("7.4.1", "ghc-7.4.1", "ghc-pkg-7.4.1")
+  , tiDefault = [ ("7.4.1", "ghc-7.4.1", "ghc-pkg-7.4.1")
+                , ("7.6.3", "ghc-7.6.3", "ghc-pkg-7.6.3")
+                , ("7.8.3", "ghc-7.8.3", "ghc-pkg-7.8.3") ]
+  , tiTest = []
+  }
+
+main = runTests inputs
