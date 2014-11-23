cbfeeb4aa59c5ae2ad5c746c9cc8e9fbe5a4fb81
Omari Norman <omari@smileystation.com>
2014-11-23 08:50:06 -0500

    bump dependency versions

--- CabalCommon.hs
+++ CabalCommon.hs
@@ -3,7 +3,7 @@ module CabalCommon where
 import qualified Cartel as A
 
 versionInts :: [Int]
-versionInts = [0,20,0,0]
+versionInts = [0,20,0,2]
 
 -- Package version
 version :: A.Version
@@ -18,7 +18,7 @@ terminfo :: A.Package
 terminfo = A.closedOpen "terminfo" [0,3,2] [0,5,0,0]
 
 text :: A.Package
-text = A.closedOpen "text" [0,11,2,0] [1,2,0,0]
+text = A.closedOpen "text" [0,11,2,0] [1,3,0,0]
 
 barecheck :: A.Package
 barecheck = A.closedOpen "barecheck" [0,2,0,0] [0,3]
